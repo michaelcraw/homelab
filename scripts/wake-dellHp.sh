0d2fe667277b8f95be29d8892a61fce0eeec1e7c
michael crawford <michael.crawford@du.edu>
2026-06-19 18:00:16 -0600

    Add 5070 WOL to wake script

--- scripts/wake-dellHp.sh
+++ scripts/wake-dellHp.sh
@@ -8,3 +8,6 @@ echo "waking HP NAS"
 ssh michael@100.96.120.65 "wakeonlan -i 10.0.0.255 6c:3b:e5:3a:e2:81"
 echo "[hp-nas] wake packet sent"
 
+echo "waking Dell 5070"
+ssh michael@100.96.120.65 "wakeonlan -i 10.0.0.255 00:4e:01:bc:04:ba"
+echo "[5070] wake packet sent"
